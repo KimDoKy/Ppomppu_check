1edd31025a91fbe4e980eacdded419d98981d936
Kim Do Ky <makingfunk0@gmail.com>
2019-01-23 20:00:13 +0900

    backend / write test for crawlingdata model / change model options (image -> blank,null True)

--- ppomppu/crawling_data/tests.py
+++ ppomppu/crawling_data/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import CrawlingData as CD
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+
+    def setUp(self):
+        self.test_title = "Test title"
+        self.test_category = "Test Category"
+        self.test_date = "Test Date"
+        self.test_link = "Test Link"
+        self.test_instance = CD(title=self.test_title,
+                                category=self.test_category,
+                                write_date=self.test_date,
+                                detail_link=self.test_link)
+
+    def test_model_can_create_a_crawling_data(self):
+        old_count = CD.objects.count()
+        self.test_instance.save()
+        new_count = CD.objects.count()
+        self.assertNotEqual(old_count, new_count)
