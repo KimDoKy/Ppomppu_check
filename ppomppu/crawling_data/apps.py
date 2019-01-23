5770a033e7207fb476b9c1aae9f59790810220cd
Kim Do Ky <makingfunk0@gmail.com>
2019-01-23 19:45:56 +0900

    backend / migrate CrawlingData model

---
+++ ppomppu/crawling_data/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrawlingDataConfig(AppConfig):
+    name = 'crawling_data'
---
+++ ppomppu/crawling_data/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class CrawlingData(models.Model):
+    title = models.TextField()
+    category = models.CharField(max_length=10)
+    write_date = models.CharField(max_length=10)
+    detail_link = models.URLField()
+    prod_image = models.ImageField()
+    crawling_data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}'
