acd879c3290b01f65ab743ec16118866884a3d5f
Kim Do Ky <makingfunk0@gmail.com>
2019-01-23 19:07:16 +0900

    crawling / 키워드 매칭

--- crawling.py
+++ crawling.py
@@ -8,6 +8,8 @@ detail_url = 'http://www.ppomppu.co.kr/zboard/view.php?id='
 
 page_num = 5 # 하루 평균 게시판 4페이지 정도 올라옴
 
+keyword = input('키워드를 입력하세요. ')
+
 def craw_item(url):
     html = requests.get(url)
     print(html.status_code)
@@ -17,6 +19,7 @@ def craw_item(url):
     for num, item in enumerate(items):
         # menu, 공지사항 제외
         if num == 0 or num == 1: continue
+        elif re.search(keyword, item.find('font').text) == None: continue 
         print('----------', str(num), '----------')
         print('category: ', item.find('nobr').text)
         print('title: ', item.find('font').text)
