55d16c1ca59291af4757c10c13fb109d9b0712b7
Kim Do Ky <makingfunk0@gmail.com>
2019-01-25 12:47:50 +0900

    backend / 자잘한 수정

--- ppomppu/crawling_data/views.py
+++ ppomppu/crawling_data/views.py
@@ -2,8 +2,6 @@ from django.http import HttpResponse
 from .utils.crawling import craw_item
 from .utils.save_db import save_db
 
-detaile_url = 'http://www.ppomppu.co.kr/zboard/view.php?id='
-
 def run_crawling(request):
     url = 'http://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu&page=1'
     contents = craw_item(url)
