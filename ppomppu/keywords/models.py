93fa9bfe5b80093efcd8d16ab134d9ffdfdf85fe
Kim Do Ky <makingfunk0@gmail.com>
2019-01-27 17:03:24 +0900

    keywords / update_link 추가, owner에 related_name 추가

--- ppomppu/keywords/models.py
+++ ppomppu/keywords/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 class Keywords(models.Model):
     keyword = models.CharField(max_length=100)
-    owner = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
+    owner = models.ForeignKey('users.CustomUser', related_name='keywords', on_delete=models.CASCADE)
     alarm = models.BooleanField(default=True)
+    update_link = models.ForeignKey('crawling_data.CrawlingData', blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.keyword}'
