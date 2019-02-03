9f407c6c458ad42033cf32b3b3ba398139f47b0b
Kim Do Ky <makingfunk0@gmail.com>
2019-02-03 15:28:40 +0900

    write user testcase

--- ppomppu/users/tests.py
+++ ppomppu/users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import CustomUser as CU
 
-# Create your tests here.
+def UserCreateTestCase(TestCase):
+
+    def setUp(self):
+        self.username = 'test1'
+        self.email = 'test1@test.com'
+
+    def test_create_user(self):
+        old_count = CU.objects.count()
+        CU.objects.create(username=self.username,
+                          email=self.email)
+        new_count = CU.objects.count()
+        self.assertNotEqual(old_count, new_count)
+
