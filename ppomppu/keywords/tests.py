c74d31f43b8f278a98ffedcc7900a26622e399f7
Kim Do Ky <makingfunk0@gmail.com>
2019-02-03 15:31:56 +0900

    update keyword testcase

--- ppomppu/keywords/tests.py
+++ ppomppu/keywords/tests.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .models import Keywords
-
 from users.models import CustomUser as CU
 
 class KeyWordsTestCase(TestCase):
 
     def setUp(self):
         self.keyword = 'test keyword'
-        self.owner = CU.objects.create(name='tester1')
+        print('test keyword')
+        self.u1 = CU.objects.create(email='test@test.com')
+        self.owner = self.u1
         self.test_instance = Keywords(keyword=self.keyword,
                                       owner=self.owner)
 
