08acc201faf65ad69d315ba44d6e4056fac4d8ad
Kim Do Ky <makingfunk0@gmail.com>
2019-02-13 16:04:50 +0900

    send_mail / send mail when CrawlingData model save DB

--- ppomppu/crawling_data/models.py
+++ ppomppu/crawling_data/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class CrawlingData(models.Model):
     title = models.TextField()
@@ -11,3 +13,11 @@ class CrawlingData(models.Model):
 
     def __str__(self):
         return f'{self.title}'
+
+from .utils.send_mail import send_mails
+
+@receiver(post_save, sender=CrawlingData)
+def updata_signal(sender, instance, **kwargs):
+    print('---signal-----')
+    print(instance)
+# send_mails('abmu333@hanmail.net')
