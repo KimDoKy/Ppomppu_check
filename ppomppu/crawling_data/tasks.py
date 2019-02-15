40d79a685f6d59ba739639fa53453e6fc5efbf26
Kim Do Ky <makingfunk0@gmail.com>
2019-02-15 12:47:21 +0900

    backend / task에서 정상 동작으로 되돌림

--- ppomppu/crawling_data/tasks.py
+++ ppomppu/crawling_data/tasks.py
@@ -11,21 +11,9 @@ from .utils.send_mail import send_mails
 
 
 def matching_keyword(update_key, keywords):
-    print(update_key.title)
     for key in keywords:
-        print(key.keyword)
-        print(os.getpid())
         if re.search(key.keyword, update_key.title):
             send_mails(key, update_key)
-
-def multi_proc(keywords, update_data):
-    procs = []
-    for update_key in update_data:
-        proc = Process(target=matching_keyword, args=(update_key, keywords))
-        procs.append(proc)
-        proc.start()
-    for proc in procs:
-        proc.join()
 
 @task
 def crawling():
@@ -36,4 +24,5 @@ def crawling():
     # update_data는 redis를 통해 부하를 줄일 수 있을 것 같다.
     # 릭팩토링 예정
     update_data = CrawlingData.objects.filter(status=True)
-    multi_proc(keywords, update_data)
+    for update_key in update_data:
+        matching_keyword(update_key, keywords)
