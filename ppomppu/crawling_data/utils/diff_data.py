7c166af851d2b19162b662720af994184e5da776
Kim Do Ky <makingfunk0@gmail.com>
2019-08-23 13:10:16 +0900

    crawling_data / code refactoring

--- ppomppu/crawling_data/utils/diff_data.py
+++ ppomppu/crawling_data/utils/diff_data.py
@@ -14,15 +14,17 @@ def diff_data(contents, host):
     # celery 캐시 데이터의 갯수를 가지고와서
     # 최근순으로 읽어들인다.
     # 모두 오류이거나 없다면 cache_title은 빈 리스트를 반환한다.
-    if conn.keys('celery*'):
-        key_length = len(conn.keys('celery*'))
+    cache = conn.keys('celery*')
+    if cache:
+        key_length = len(cache)
         for i in range(key_length - 1, 0, -1):
-            before_cache = json.loads(conn.get(conn.keys('celery*')[i]))
+            before_cache = json.loads(conn.get(cache[i]))
             if before_cache['status'] == 'FAILURE':
                 pass
-            before_cache = before_cache['result']
-            for key in before_cache:
-                cache_title.append(before_cache[key]['title'])
+            else:
+                before_cache_result = before_cache['result']
+                for key in before_cache_result:
+                    cache_title.append(before_cache_result[key]['title'])
     # 캐시 데이터와 업데이트 데이터를 비교한다.
     # return -> dict
     for key in contents:
