be112dff96dbae7d4bf191161eb4e8ac26b8e1a3
Kim Do Ky <makingfunk0@gmail.com>
2019-08-23 13:09:06 +0900

    crawling_data / 발송 메일양식 수정 / title 추가 및 이미지 추가

--- ppomppu/crawling_data/utils/send_mail.py
+++ ppomppu/crawling_data/utils/send_mail.py
@@ -7,21 +7,23 @@ def send_mails(key, instance):
     keyword = key.keyword
     detail_link = instance['detail_link']
     prod_img = instance['prod_image']
+    title = instance['title']
     id = settings.CONF_FILES['email']['id']
     pw = settings.CONF_FILES['email']['password']
     body = '''
         <center>
-        <h3>
-        [뽐뿌 Checker] <a href="{0}"><h2>{1}</h2></a>
+        <h3>[뽐뿌 Checker]</h3>
+        <br>
+        지정한 키워드: <b><a href="{0}">{1}</b></a>
         게시물이 새로 등록되었습니다.
-        </h3>
         <br><br>
-        <img src="{2}" width=200px>
+        <a href="{0}">{2}</a>
+        <img src="http://{3}">
         <br><br>
         <hr>
         <a href="https://app.pycon.shop"><img src="https://app.pycon.shop/img/logo.135e5f07.png" width=100px></a>
         </center>
-        '''.format(detail_link, keyword, prod_img)
+        '''.format(detail_link, keyword, title, prod_img)
     msg = MIMEText(body, 'html')
     msg['Subject'] = '[뽐뿌 Checker] ' +  keyword + ' 새로운 게시물이 등록되었습니다.'
     msg['To'] = address
