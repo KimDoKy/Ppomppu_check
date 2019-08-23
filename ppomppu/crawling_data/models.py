5f7fdcddc6cd3c3ec6ca87e9dd15c745a58d93f4
Kim Do Ky <makingfunk0@gmail.com>
2019-08-23 13:07:41 +0900

    crawling_data / 크롤링후 DB에 저장할때 메일 발송 기능이 작동하도록 signal 추가

--- ppomppu/crawling_data/models.py
+++ ppomppu/crawling_data/models.py
@@ -14,3 +14,15 @@ class CrawlingData(models.Model):
     def __str__(self):
         return f'{self.title}'
 
+from .utils.send_mail import send_mails
+from keywords.models import Keywords
+import re
+
+@receiver(post_save, sender=CrawlingData)
+def update_signal(sender, instance, **kwargs):
+    if instance.status == True:
+        keywords = Keywords.objects.filter(alarm=True)
+        for key in keywords:
+            keyword = key.keyword
+            if re.search(keyword, instance.title):
+                send_mails(key, instance)
