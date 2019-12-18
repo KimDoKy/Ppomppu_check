36a7905c8cf95db3096a5604cdc7da392e84832d
Kim Do Ky <makingfunk0@gmail.com>
2019-12-18 23:03:48 +0900

    set sentry

--- ppomppu/conf/settings/base.py
+++ ppomppu/conf/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'django.contrib.sites',
     'corsheaders',
+    'raven.contrib.django.raven_compat',
     'crawling_data',
     'users',
     'keywords',
@@ -183,3 +184,56 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 
+# Sentry
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+import raven
+
+sentry_sdk.init(
+    dsn=CONF_FILE['SENTRY']['dsn'],
+    integrations=[DjangoIntegration()]
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR', # To capture more than ERROR, change to WARNING, INFO, etc.
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'root': {
+            'level': 'WARNING',
+            'handlers': ['sentry'],
+        },
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
